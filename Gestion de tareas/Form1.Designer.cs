f4cff4875a7c918fd26dd3d797748f2e51598098
EduardoMartine <whatdice26@gmail.com>
2024-08-14 16:19:12 -0400

    Initial commit

---
+++ Gestion de tareas/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Gestion_de_tareas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel3 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel4 = new System.Windows.Forms.ToolStripLabel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.toolStripLabel5 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripLabel2,
+            this.toolStripLabel3,
+            this.toolStripLabel4,
+            this.toolStripLabel5});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(551, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(63, 22);
+            this.toolStripLabel1.Text = "Agregar";
+            this.toolStripLabel1.Click += new System.EventHandler(this.toolStripLabel1_Click);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(63, 22);
+            this.toolStripLabel2.Text = "Eliminar";
+            this.toolStripLabel2.Click += new System.EventHandler(this.toolStripLabel2_Click);
+            // 
+            // toolStripLabel3
+            // 
+            this.toolStripLabel3.Name = "toolStripLabel3";
+            this.toolStripLabel3.Size = new System.Drawing.Size(62, 22);
+            this.toolStripLabel3.Text = "Guardar";
+            this.toolStripLabel3.Click += new System.EventHandler(this.toolStripLabel3_Click);
+            // 
+            // toolStripLabel4
+            // 
+            this.toolStripLabel4.Name = "toolStripLabel4";
+            this.toolStripLabel4.Size = new System.Drawing.Size(126, 22);
+            this.toolStripLabel4.Text = "Personalizar color";
+            this.toolStripLabel4.Click += new System.EventHandler(this.toolStripLabel4_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(28, 151);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(282, 84);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Descripción";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(25, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Nom.Tarea";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(363, 183);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(103, 20);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Completada";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(28, 281);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(419, 148);
+            this.listBox1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 262);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 18);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Tareas";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(453, 340);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Mostrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(28, 91);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(191, 24);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_Load);
+            // 
+            // toolStripLabel5
+            // 
+            this.toolStripLabel5.Name = "toolStripLabel5";
+            this.toolStripLabel5.Size = new System.Drawing.Size(111, 22);
+            this.toolStripLabel5.Text = "Cambiar fuente";
+            this.toolStripLabel5.Click += new System.EventHandler(this.toolStripLabel5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(551, 464);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Lista de Tarea";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel3;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel5;
+    }
+}
+
---
+++ Gestion de tareas/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Gestion_de_tareas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+            SqlConnection cone = new SqlConnection("server= ALDO\\SQLEXPRESS; database=ListaDeTareas; integrated security= true;");
+            cone.Open();
+            MessageBox.Show("Conectado");
+            string tarea = comboBox1.Text;
+            string descri = richTextBox1.Text;
+
+
+            string cadena = "Insert into Tareas(Tarea,Descripcion,Estado) values('" + tarea + "','" + descri + "','Pendiente')";
+            SqlCommand comand = new SqlCommand(cadena, cone);
+            comand.ExecuteNonQuery();
+            cone.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection cone = new SqlConnection("server= ALDO\\SQLEXPRESS; database=ListaDeTareas; integrated security= true;");
+            cone.Open();
+
+            string query = "SELECT Tarea,Descripcion,Estado FROM Tareas";
+            SqlCommand comand = new SqlCommand(query, cone);
+            SqlDataReader reader = comand.ExecuteReader();
+            listBox1.Items.Clear();
+            while (reader.Read())
+            {
+
+                string tareaInfo = $"{reader["Tarea"].ToString()}{reader["Descripcion"].ToString()}{reader["Estado"].ToString()}";
+
+
+                listBox1.Items.Add(tareaInfo);
+            }
+
+        }
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            string nombre = comboBox1.Text;
+            SqlConnection cone = new SqlConnection("server= ALDO\\SQLEXPRESS; database=ListaDeTareas; integrated security= true;");
+            cone.Open();
+            string query2 = "UPDATE Tareas SET Estado = 'Completado' WHERE Tarea = '" + nombre + "'";
+            SqlCommand comando = new SqlCommand(query2, cone);
+            comando.ExecuteNonQuery();
+            cone.Close();
+
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged_Load(object sender, EventArgs e)
+        {
+            comboBox1.Text = string.Empty;
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            SqlCommand comand = new SqlCommand();
+            DataTable tabla = new DataTable();
+
+            SqlConnection cone = new SqlConnection("server=ALDO\\SQLEXPRESS; database=ListaDeTareas; integrated security=true;");
+            cone.Open();
+            comand.Connection = cone;
+            comand.CommandType = CommandType.Text;
+            comand.CommandText = "SELECT Tarea FROM Tareas";
+            SqlDataAdapter adapter = new SqlDataAdapter(comand);
+            adapter.Fill(tabla);
+
+            comboBox1.DataSource = tabla;
+            comboBox1.DisplayMember = "Tarea";
+            comboBox1.ValueMember = "Tarea";
+        }
+
+        private void toolStripLabel2_Click(object sender, EventArgs e)
+        {
+            string nombre = comboBox1.Text;
+            SqlConnection cone = new SqlConnection("server= ALDO\\SQLEXPRESS; database=ListaDeTareas; integrated security= true;");
+            cone.Open();
+            string query3 = "DELETE FROM Tareas WHERE Tarea = '" + nombre + "'"; ;
+            SqlCommand comando = new SqlCommand(query3, cone);
+            comando.ExecuteNonQuery();
+            MessageBox.Show("Eliminado");
+            cone.Close();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripLabel4_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+
+            
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                
+                Color colorSeleccionado = colorDialog.Color;
+
+                
+                this.BackColor = colorSeleccionado;
+            }
+        }
+
+        private void toolStripLabel5_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+
+            
+            if (fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                
+                Font fuenteSeleccionada = fontDialog.Font;
+
+                
+                listBox1.Font = fuenteSeleccionada;
+            }
+        }
+
+        private void toolStripLabel3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
